c37de57a2848d27e7281dc97040cc474b0cd3f2e
Marian Koreniuk <moriarti@cp.if.ua>
2025-10-03 15:34:55 +0200

    core: remove backend_manager import/exports from core/mod.zig

--- src/core/mod.zig
+++ src/core/mod.zig
@@ -4,7 +4,6 @@ pub const interfaces = @import("interfaces.zig");
 pub const errors = @import("errors.zig");
 pub const logging = @import("logging.zig");
 pub const config = @import("config.zig");
-pub const backend_manager = @import("backend_manager.zig");
 
 // Re-export commonly used types
 pub const Error = types.Error;
@@ -20,4 +19,3 @@ pub const LogLevel = logging.LogLevel;
 pub const LogContext = logging.LogContext;
 pub const Config = config.Config;
 pub const ConfigLoader = config.ConfigLoader;
-pub const BackendManager = backend_manager.BackendManager;
